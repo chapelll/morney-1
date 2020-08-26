85a29c1d9506f831c83300db04aa5f14c4d7a97b
chapelll <214426857@qq.com>
2020-08-26 15:45:15 +0800

    三个页面布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        LABELS
+    <div class="nav-wrapper">
+        <div class="content">
+            <div>Labels</div>
+
+        </div>
         <Nav></Nav>
     </div>
 </template>
@@ -12,5 +15,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper {
+        display: flex;
+        flex-direction: column;
+        border: 1px solid green;
+        height: 100vh;
+    }
+    .content {
+        overflow: auto;
+        flex-grow: 1;
+        border: 1px solid blue;
+    }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,21 +1,29 @@
 <template>
-    <div>
-        MONEY
+    <div class="nav-wrapper">
+        <div class="content">
+            <div>money</div>
+
+        </div>
         <Nav></Nav>
     </div>
 </template>
 
 <script lang="ts">
-import Nav from '@/components/Nav.vue'
-
     export default {
         name: 'Money',
-        components: {
-            Nav
-        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper {
+        display: flex;
+        flex-direction: column;
+        border: 1px solid green;
+        height: 100vh;
+    }
+    .content {
+        overflow: auto;
+        flex-grow: 1;
+        border: 1px solid blue;
+    }
 </style>
