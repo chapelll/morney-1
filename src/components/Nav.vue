0c005de44dd524326145cbbcfea18362ab20be40
chapelll <214426857@qq.com>
2020-08-28 14:33:02 +0800

    添加全局样式、变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
    <nav>
-      <router-link to="/money" class="item">
+      <router-link to="/money" class="item" active-class="selected">
         <Icon name="money"></Icon>
         记账
       </router-link>
-      <router-link to="/labels" class="item">
+      <router-link to="/labels" class="item" active-class="selected">
         <Icon name="label"></Icon>
         标签
       </router-link>
-      <router-link to="/statistics" class="item">
+      <router-link to="/statistics" class="item" active-class="selected">
         <Icon name="statistics"></Icon>
         统计
       </router-link>
@@ -22,13 +22,15 @@
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/styles/helper.scss";
+
     nav {
         display: flex;
         box-shadow: 0 0 3px rgba(0,0,0,.25);
         > .item {
-            padding: 2px 0;
             display: flex;
             flex-direction: column;
+            padding: 2px 0;
             width: 33.3333%;
             align-items: center;
             justify-content: center;
@@ -37,6 +39,9 @@
                 width: 32px;
                 height: 32px;
             }
+        }
+        > .item.selected {
+            color: $color-highlight;
         }
     }
 
