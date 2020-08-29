987b4982d01db60ec2f215538c6023ab80b676b2
chapelll <214426857@qq.com>
2020-08-29 23:31:32 +0800

    模块化: 把 Money.vue 分成4个子组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tags",
+};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+        display: flex;
+        flex-direction: column-reverse;
+        flex-grow: 1;
+        font-size: 14px;
+        padding: 16px;
+        > .current {
+            display: flex;
+            flex-wrap: wrap;
+            > li {
+                background: #d9d9d9;
+                height: 24px;
+                line-height: 24px;
+                // 当只有一行字时，行高与高一样高可以使文本垂直居中
+                border-radius: 12px;
+                padding: 0 16px;
+                margin-right: 12px;
+                margin-top: 4px;
+            }
+        }
+        > .new {
+            padding-top: 16px;
+            button {
+                background: transparent;
+                border: none;
+                border-bottom: 1px solid;
+                color: #999;
+                padding: 0 4px;
+            }
+        }
+    }
+
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Types",
+};
+</script>
+
+<style lang="scss" scoped>
+
+.types {
+        display: flex;
+        background: #c4c4c4;
+        font-size: 24px;
+        text-align: center;
+        > li {
+            display: flex;
+            height: 64px;
+            width: 50%;;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            &.selected::after {
+                // &代表 li 自己 综合起来的意思就是:拥有 selected 属性的 li
+                content: '';
+                position: absolute;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                height: 4px;
+                background: #333;
+            }
+        }
+    }
+</style>
