058ddd2bb85407e5ce1cb98322778d977cbb79ca
chapelll <214426857@qq.com>
2020-09-01 16:13:25 +0800

    把record的复制保存至List

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     {{record}}
-    <number-pad @update:value="onUpdateAmount"></number-pad>
+    <number-pad @update:value="onUpdateAmount" @submit="saveRecord"></number-pad>
 
     <types :value.sync="record.type"></types>
 
@@ -17,7 +17,7 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-import { Component } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 
 // 定义一个接口,名称为Record,它规定了有哪些属性及属性的类型
 type Record = {
@@ -38,11 +38,16 @@ export default class Money extends Vue {
     notes: "",
     type: "-",
     amount: 0,
-  }; // 定义了一个对象record，它的接口是Reord，因此它必须要有Record的属性且属性数据类型要一致
+  }; 
+  // 定义了一个对象record，它的接口是Reord，因此它必须要有Record的属性且属性数据类型要一致
+
+  recordList: Record[] = [];
+  // 定义一个recordList数组用来存放 record
+
   // 方法
   onUpdateTags(value: string[]) {
     this.record.tags = value;
-  }
+  }         
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
@@ -50,6 +55,19 @@ export default class Money extends Vue {
 
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
+  }
+
+  saveRecord() {
+    // 每次调用saveRecord函数时都创建一个新的对象record2，record2的属性与record相同，将它存放进recordList
+    const record2 = JSON.parse(JSON.stringify(this.record))
+    this.recordList.push(record2)
+    // push的是record这个对象的地址，所以recordList存放的都是一样的数据
+    console.log(this.recordList);
+  }
+  // Watch  
+  @Watch('recordList')
+  OnRecordListChange() {
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
   }
 }
 </script>
