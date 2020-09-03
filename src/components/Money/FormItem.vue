89b69093ac6eb225f6f36f7e92b4e0a310deeb49
chapelll <214426857@qq.com>
2020-09-03 11:27:31 +0800

    修复 value 被赋值的问题

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,7 +2,11 @@
   <div>
     <label class="formItem">
       <span class="name">{{fieldName}}</span>
-      <input type="text" :placeholder="placeholder" v-model="value"/>
+      <input type="text" 
+      :placeholder="placeholder" 
+      :value="value"
+      @input="onValueChanged($event.target.value)"
+      />
     </label>
   </div>
 </template>
@@ -14,16 +18,17 @@ import { Component, Prop, Watch } from 'vue-property-decorator';
 @Component
 export default class FormItem extends Vue {
   //属性
-  value = ''
+  @Prop({default: ''}) readonly value!: string;
   //prop属性
   @Prop({required: true}) fieldName!: string 
   @Prop() placeholder?: string 
   // watch 
   // 当 value 的值发生改变时就触发onValueChanged函数，第一个参数是value改变后的值，第二个参数是value改变前的值
   @Watch('value') 
-  onValueChanged(value: string, oldValue: string) {
+  onValueChanged(value: string) {
     this.$emit('update:value', value)
   }
+  // 当向input框输入时触发onValueChanged事件，参数是输入的内容
   //方法
 }
 </script>
