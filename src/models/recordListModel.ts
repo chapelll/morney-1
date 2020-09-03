666fea2562bcd788c5ebf291d27b38a0a19c7fe1
chapelll <214426857@qq.com>
2020-09-03 21:46:03 +0800

    重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,17 @@
 // 将数据层面的操作进行封装(读取和写入)
 const localStorageKeyName = 'recordList'
 const recordListModel = {
+
+    data: [] as RecordItem[],
+
     //获取数据函数
     fetch() {
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[]
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[]
+        return this.data
     },
     //保存数据函数
-    save(data: RecordItem[]) {
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data))
+    save() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
     },
     //深拷贝函数
     clone(data: RecordItem | RecordItem[]) {
