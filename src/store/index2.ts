0a418a92b6b38551d89fdf84e23b857c643b99c2
chapelll <214426857@qq.com>
2020-09-04 16:24:15 +0800

    重构store之前的状态

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,41 +1,9 @@
-import tagListModel from '@/models/tagListModel'
-import recordListModel from '@/models/recordListModel'
+import tagListStore from '@/store/tagStore.ts'
+import recordStore from '@/store/recordStore.ts'
 
 const store = {
-    // record store
-    recordList: recordListModel.fetch(),
-  
-    createRecord: (record: RecordItem) => {
-      return recordListModel.create(record)
-    },
-  
-    // tag store
-    tagList: tagListModel.fetch(),
-  
-    findTag (id: string) {
-      return this.tagList.filter(t => t.id === id)[0]
-    },
-  
-    createTag: (name: string) => {
-      const message = tagListModel.create(name)
-      if (message === 'duplicated') {
-        window.alert('标签名重复')
-      } else if (message === 'success') {
-        window.alert('添加成功')
-      }
-    },
-  
-    removeTag: (id: string) => {
-      if (tagListModel.remove(id)) {
-        return true
-      } else {
-        return false
-      }
-    },
-  
-    updateTag: (id: string, name: string) => {
-      return tagListModel.update(id, name)
-    }
+    ...recordStore,
+    ...tagListStore
 }
 
 export default store;
