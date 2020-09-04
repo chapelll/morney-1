be29d479bd2a3c0f2f44134dfaf65be4e480b2b2
chapelll <214426857@qq.com>
2020-09-04 22:11:22 +0800

    Money.vue使用vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import clone from '@/lib/clone.ts'
 
 Vue.use(Vuex)
+
 const store = new Vuex.Store({
-  state: {
-    count: 0
+  state: { //data
+    recordList: [] as RecordItem[]
+    // 将recordList存储在vuex的state中(方便进行全局管理)
   },
-  mutations: {
-    increment(state, n: number) {
-      state.count += n
-    }
+  mutations: { //methods
+
+    // 获取记录列表数据
+    fetchRecords(state) {
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+      // state中的recordList需要从localStorage中取出
+    },
+    // 创建新的记录
+    createRecord(state, record) {
+      const record2: RecordItem = clone(record)
+      record2.createdAt = new Date()
+      state.recordList.push(record2)
+      store.commit('saveRecords') 
+      // 将新的记录经过深拷贝，添加到state.recordList中去，接着就调用saveRecords函数把recordList进行存储
+    },
+    // 存储记录列表
+    saveRecords(state) {
+      window.localStorage.setItem('recordList', JSON.stringify(state.recordList))
+      // 将recordList存放在localStorage中去
+    },
   }
 })
-
-console.log(store.state.count);
-store.commit('increment',10)
-console.log(store.state.count);
 
 
 export default store
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -10,7 +10,6 @@
     </div>
 
     <Tags></Tags>
-    {{xxx}} <button @click="$store.commit('increment', 10)">+1</button>
   </Layout>
 </template>
 
@@ -21,19 +20,17 @@ import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component } from "vue-property-decorator";
-import oldStore from '@/store/index2.ts';
 
 @Component({
   components: { NumberPad, Types, FormItem, Tags },
   computed: {
-    xxx() {
-      // xxx是一个计算属性，它的值依赖于 store.state.count
-      return this.$store.state.count
+    recordList() {
+      return this.$store.state.recordList;
+      // recordList映射的是vuex中的recordList映射的是vuex中的
     }
   }
 })
 export default class Money extends Vue {
-  recordList = oldStore.recordList;
   record: RecordItem = {
     tags: [],
     notes: "",
@@ -42,6 +39,10 @@ export default class Money extends Vue {
   }; 
   // 定义了一个对象record，它的接口是Reord，因此它必须要有Record的属性且属性数据类型要一致
 
+  created() {
+    // 页面创建时获得数据
+    this.$store.commit('fetchRecords')
+  }
 
   // 方法
   onUpdateNotes(value: string) {
@@ -53,7 +54,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    oldStore.createRecord(this.record)
+    this.$store.commit('createRecord', this.record)
   }
 
 
