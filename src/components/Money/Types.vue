622cd130aa2fc87ee5e649214b76fde5f37ba9d2
chapelll <214426857@qq.com>
2020-09-06 15:21:01 +0800

    封装Tabs组件之前

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' ? 'selected' : ''" @click="selectType('-')">支出</li>
-      <li :class="value === '+' ? 'selected' : ''" @click="selectType('+')">收入</li>
+      <li :class="{[classPrefix + '-item']: classPrefix, selected: value === '-'}" @click="selectType('-')">支出</li>
+      <li :class="{[classPrefix + '-item']: classPrefix, selected: value === '+'}" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
@@ -15,6 +15,7 @@ import { Component, Prop, Watch } from 'vue-property-decorator';
 export default class Types extends Vue {
   // prop属性
   @Prop(String) value!: string
+  @Prop(String) classPrefix?: string
   // 方法
   selectType(type: string) {
     this.$emit('update:value', type)
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,15 +1,31 @@
 <template>
-        <Layout>
-            Statistics
-        </Layout>
+  <Layout>
+    <Types class-prefix="zzz" :value.sync="type"></Types>Statistics
+  </Layout>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Statistics',
-    }
+import Vue from "vue";
+import Types from "@/components/Money/Types.vue";
+import { Component } from "vue-property-decorator";
+
+@Component({
+  components: { Types },
+})
+export default class Statistics extends Vue {
+  // 属性
+  type = "-";
+}
 </script>
 
 <style lang="scss" scoped>
-    
+::v-deep .zzz-item {
+  background: white;
+  &.selected {
+    background: #c4c4c4;
+    &::after {
+      display: none;
+    }
+  }
+}
 </style>
